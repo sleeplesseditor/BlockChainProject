343fcca120b6b338a144696c8801c4e6baa05526
sleeplesseditor <josh@bryson.me.uk>
2018-02-15 18:48:04 +0000

    Express app created with get and post requests

---
+++ app/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const BlockChain = require('../blockchain');
+
+//Option for user choosing different port
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
+
+const app = express();
+const bc = new BlockChain();
+
+app.use(bodyParser.json());
+
+app.get('/blocks', (req, res) => {
+    res.json(bc.chain);
+});
+
+app.post('/mine', (req, res) => {
+    const block = bc.addBlock(req.body.data);
+    console.log(`New block added: ${block.toString()}`);
+
+    res.redirect('/blocks');
+});
+
+app.listen(HTTP_PORT, () => {
+    console.log(`Listening on port ${HTTP_PORT}`)
+});
