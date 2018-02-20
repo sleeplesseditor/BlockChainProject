6b4ad02bff282651bc9f2c7f7b18d41faa65fc38
sleeplesseditor <josh@bryson.me.uk>
2018-02-20 17:25:57 +0000

    Transaction Pool Feature Created

--- app/index.js
+++ app/index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const BlockChain = require('../blockchain');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 //Option for user choosing different port
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new BlockChain();
-const p2pServer = new P2pServer(bc);
+const wallet = new Wallet();
+const tp = new TransactionPool();
+const p2pServer = new P2pServer(bc, tp);
 
 app.use(bodyParser.json());
 
@@ -24,6 +28,24 @@ app.post('/mine', (req, res) => {
     p2pServer.syncChains();
 
     res.redirect('/blocks');
+});
+
+//Returns transactions within user's transaction pool instance
+app.get('/transactions', (req, res) => {
+    res.json(tp.transactions);
+});
+
+//Adds new transactions to user's transaction pool instance
+app.post('/transact', (req, res) => {
+    const { recipient, amount } = req.body;
+    const transaction = wallet.createTransaction(recipient, amount, tp);
+    p2pServer.broadcastTransaction(transaction);
+    res.redirect('/transactions');
+});
+
+//Public Key Endpoint
+app.get('/public-key', (req, res) => {
+    res.json({ publicKey: wallet.publicKey });
 });
 
 app.listen(HTTP_PORT, () => {
